69b5e12a02aaf7a23fbf569220c2add100f513d8
Xiaochun Shen <snowdonshen@gmail.com>
2011-08-29 15:46:51 +0800

    Initial Commit

---
+++ HypnoTime/HypnoTimeAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HypnoTimeAppDelegate.h
+//  HypnoTime
+//
+//  Created by  Chuns on 11-8-29.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoTimeAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
